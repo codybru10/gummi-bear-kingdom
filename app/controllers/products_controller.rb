f10eb89608b28407c32508c1c7648abe77b08b51
Cody Brubaker <cody.brubaker@gmail.com>
2017-02-10 08:56:46 -0800

    add edit and update functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,20 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
-    @product.new
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :cost, :origin)
   end
 
 end
