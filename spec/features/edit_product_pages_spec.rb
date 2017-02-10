3b516d6d36c11224106cafff59ccc59050a925f9
Cody Brubaker <cody.brubaker@gmail.com>
2017-02-10 10:50:04 -0800

    add edit product integration spec

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    test_product = Product.create(:name => "Yummy Bears", :cost => 3, :origin => "Mexice", :img => "https://images.vat19.com/covers/large/worlds-largest-gummy-bear-bubblegum.jpg")
+    visit product_path(test_product)
+    click_link "Edit"
+    fill_in 'Name', :with => "Yummy Bears"
+    fill_in 'Cost', :with => 3
+    fill_in 'Origin', :with => "Mexico"
+    click_on 'Update Product'
+    expect(page).to have_content 'Yummy Bears'
+  end
+end
