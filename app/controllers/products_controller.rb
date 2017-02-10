76e8444a199ce3c9b80079d6c952b5a1bfb9aa18
Cody Brubaker <cody.brubaker@gmail.com>
2017-02-10 09:04:32 -0800

    add new product funtionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,19 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
