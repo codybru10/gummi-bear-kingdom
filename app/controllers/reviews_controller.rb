999cc45f6eaabe8d4378f129a418f468a4b46914
Cody Brubaker <cody.brubaker@gmail.com>
2017-02-10 11:51:41 -0800

    add and delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,30 @@
 class ReviewsController < ApplicationController
-  
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:review, :author)
+  end
+
 end
